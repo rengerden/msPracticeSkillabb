9635939d8f371b138ff1cb77b08379061cfa921e
Alfonso Chavez <alfonso.chavez.a@gmail.com>
2022-09-18 16:23:45 -0500

    add new configurations and modify docker files

--- msklb-p-itmt-denv-discovery-provider/src/main/java/com/skillabb/discovery/provider/MsklbPItmtDenvDiscoveryProviderApplication.java
+++ msklb-p-itmt-denv-discovery-provider/src/main/java/com/skillabb/discovery/provider/MsklbPItmtDenvDiscoveryProviderApplication.java
@@ -2,8 +2,10 @@ package com.skillabb.discovery.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsklbPItmtDenvDiscoveryProviderApplication {
 
 	public static void main(String[] args) {
